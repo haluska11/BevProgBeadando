1c15c3601a66ce7ed3fd8bc1e9ffdfed6ef7a5d6
haluska11 <60772676+haluska11@users.noreply.github.com>
2020-05-04 15:34:28 +0200

    Update Tranzitiv_fuggoseg.java
    Függőségek kialakítása

--- Bevprog_TranzitivFuggoseg/src/com/company/Tranzitiv_fuggoseg.java
+++ Bevprog_TranzitivFuggoseg/src/com/company/Tranzitiv_fuggoseg.java
@@ -33,3 +33,31 @@ public class Tranzitiv_fuggoseg {
         return alapfuggoseg.put(key, fuggosegek) != null;
     }
 
+    public Map<String, HashSet<String>> getFuggosegek() {
+        Map<String, HashSet<String>> tranzitivfuggosegek = new HashMap<>();
+        for (Entry<String, HashSet<String>> entry : alapfuggoseg.entrySet()) {
+            String kulcs = entry.getKey(); //kulcs a betű amihez képest a többi függ pl.: A
+            Set<String> fuggesakulcshoz = entry.getValue();
+            Queue<String> fuggesekQueue = new LinkedList<String>();
+            if (fuggesakulcshoz != null) {
+                fuggesekQueue.addAll(fuggesakulcshoz);
+            }
+            HashSet<String> osszesfuggesek = new HashSet<String>(); //a konkrét függések listáját jeleníti meg (A függ B,C)
+            Queue<String> segedfuggosegQueue = new LinkedList<String>();
+            while (!fuggesekQueue.isEmpty()) {
+                for (String d : fuggesekQueue) {
+                    osszesfuggesek.add(d);
+                    if (alapfuggoseg.get(d) != null) {
+                        for (String s : alapfuggoseg.get(d)) {
+                            segedfuggosegQueue.add(s);
+                        }
+                    }
+                }
+                fuggesekQueue = segedfuggosegQueue;
+                segedfuggosegQueue = new LinkedList<String>();
+            }
+            tranzitivfuggosegek.put(kulcs, osszesfuggesek);
+        }
+        return tranzitivfuggosegek;
+    }
+}
